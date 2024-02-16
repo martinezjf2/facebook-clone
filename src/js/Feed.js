366850148657a4c224ad7ed449cda1354cfe9e5c
Jeffrey Martinez <martinezjf2@gmail.com>
2024-02-15 22:40:18 -0500

    added stories on the facebook clone

--- src/js/Feed.js
+++ src/js/Feed.js
@@ -16,6 +16,76 @@ export default function Feed() {
             </div>
           </div>
         </div>
+        {/* Camila Cabello */}
+        <div className="video-container plain">
+          <img
+            className="bg"
+            src="https://i.pinimg.com/originals/32/cf/be/32cfbe96b0608c1dc67728499e7b3fa9.gif"
+          />
+          <div className="title">
+            <img
+              className="user"
+              src="https://randomuser.me/api/portraits/women/95.jpg"
+            />
+            Cindy Bane
+          </div>
+        </div>
+        {/* Camila Cabello */}
+        <div className="video-container plain">
+          <img
+            className="bg"
+            src="https://i.pinimg.com/originals/32/cf/be/32cfbe96b0608c1dc67728499e7b3fa9.gif"
+          />
+          <div className="title">
+            <img
+              className="user"
+              src="https://randomuser.me/api/portraits/women/95.jpg"
+            />
+            Cindy Bane
+          </div>
+        </div>
+        {/* Camila Cabello */}
+        <div className="video-container plain">
+          <img
+            className="bg"
+            src="https://i.pinimg.com/originals/32/cf/be/32cfbe96b0608c1dc67728499e7b3fa9.gif"
+          />
+          <div className="title">
+            <img
+              className="user"
+              src="https://randomuser.me/api/portraits/women/95.jpg"
+            />
+            Cindy Bane
+          </div>
+        </div>
+        {/* Camila Cabello */}
+        <div className="video-container plain">
+          <img
+            className="bg"
+            src="https://i.pinimg.com/originals/32/cf/be/32cfbe96b0608c1dc67728499e7b3fa9.gif"
+          />
+          <div className="title">
+            <img
+              className="user"
+              src="https://randomuser.me/api/portraits/women/95.jpg"
+            />
+            Cindy Bane
+          </div>
+        </div>
+        {/* Camila Cabello */}
+        <div className="video-container plain">
+          <img
+            className="bg"
+            src="https://i.pinimg.com/originals/32/cf/be/32cfbe96b0608c1dc67728499e7b3fa9.gif"
+          />
+          <div className="title">
+            <img
+              className="user"
+              src="https://randomuser.me/api/portraits/women/95.jpg"
+            />
+            Cindy Bane
+          </div>
+        </div>
       </div>
     </section>
   );
